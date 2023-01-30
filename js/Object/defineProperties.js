698f7b242fdbb062f9ebae5d6a93c5defd855307
Adolfo Cruz <adolfo@rewrite.com.br>
2023-01-30 09:13:23 -0400

    main

---
+++ js/Object/defineProperties.js
@@ -0,0 +1,14 @@
+// Examples
+// Using Object.defineProperties
+const obj = {};
+Object.defineProperties(obj, {
+  'property1': {
+    value: true,
+    writable: true,
+  },
+  'property2': {
+    value: 'Hello',
+    writable: false,
+  },
+});
+console.log(obj.property2);
