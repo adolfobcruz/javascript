3f1ff66361fb71c74904f2933e893ec17fcaa68c
Adolfo Cruz <adolfo@rewrite.com.br>
2023-02-06 09:08:54 -0400

    main

---
+++ js/Object/getOwnPropertySymbols.js
@@ -0,0 +1,12 @@
+const object1 = {};
+const a = Symbol('a');
+const b = Symbol.for('b');
+
+object1[a] = 'localSymbol';
+object1[b] = 'globalSymbol';
+
+const objectSymbols = Object.getOwnPropertySymbols(object1);
+
+console.log(objectSymbols.length);
+console.log(objectSymbols);
+console.log(objectSymbols[0]);
