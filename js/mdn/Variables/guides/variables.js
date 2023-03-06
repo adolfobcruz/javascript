49b2c4546cd530b864996e3b870c9e97294a674b
Adolfo Cruz <adolfo@rewrite.com.br>
2023-03-06 09:31:40 -0400

    main

---
+++ js/mdn/Variables/guides/variables.js
@@ -0,0 +1,18 @@
+// let myName;
+// let myAge;
+
+// myName = 'Adolfo';
+// myAge = 37;
+
+// console.log(myName, myAge);
+
+// A note about var
+myName = 'Adolfo';
+
+function logName() {
+  console.log(myName);
+}
+
+logName();
+
+var myName;
